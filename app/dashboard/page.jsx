00f24552eeacce1977bbe0d5aa3f10962dc372c6
Deekshitha-Anand <deeksgithub@gmail.com>
2025-11-25 23:13:25 +0530

    landing page and login page basic - done

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,7 @@
+﻿export default function Dashboard() {
+    return (
+        <div className="container">
+            This is my Dashboard!
+        </div>
+    )
+}
---
+++ app/login/page.jsx
@@ -0,0 +1,81 @@
+﻿"use client";
+
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {Card, CardHeader, CardContent, CardTitle} from "@/components/ui/card";
+import {supabase} from "@/lib/supabase";
+import {useRouter} from "next/navigation";
+import {toast} from "sonner";
+
+export default function LoginPage() {
+    const router = useRouter();
+    const [loading, setLoading] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+        const { data, error } = await supabase.auth.signInWithPassword({email, password,});
+
+        if (error) {
+            setLoading(false);
+            return;
+        }
+
+        toast.success("Login success!", data.user);
+        router.push("/dashboard")
+
+        const {data: { user },} = await supabase.auth.getUser();
+        console.log("Fetched user from auth.users:", user);
+        setLoading(false);
+    };
+
+    return (
+        <div className="min-h-screen grid grid-cols-1 md:grid-cols-2">
+            {/* Left Logo */}
+            <div className="flex items-center justify-center bg-gray-100">
+                <h1 className="text-4xl font-bold">MyLogo</h1>
+            </div>
+
+            {/* Right Login */}
+            <div className="flex items-center justify-center p-6">
+                <Card className="w-full max-w-md">
+                    <CardHeader>
+                        <CardTitle>Login</CardTitle>
+                    </CardHeader>
+
+                    <CardContent>
+                        <form onSubmit={handleLogin} className="space-y-4">
+                            <Input
+                                type="email"
+                                placeholder="Email"
+                                disabled={loading}
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+
+                            <Input
+                                type="password"
+                                placeholder="Password"
+                                disabled={loading}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+
+                            <Button
+                                type="submit"
+                                className="w-full"
+                                disabled={loading || !email || !password}
+                            >
+                                {loading ? "Logging in..." : "Login"}
+                            </Button>
+                        </form>
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
+    );
+}
